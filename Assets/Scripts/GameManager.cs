595df242df4ccb1fba321a11f7ae374f2d4dd6a7
Christian James <cjames6305@sdsu.edu>
2021-02-23 17:38:04 -0800

    Add bibliography, Character Sheet, and Roll Character button scene loading

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+
+    bool rolledCharacter = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,9 +19,24 @@ public class GameManager : MonoBehaviour
         
     }
 
-    public void onExitClicked()
+    public void onPlayClicked()
+    {
+        if (rolledCharacter)
+            Debug.Log("On Play Button Click");
+        else
+            Debug.Log("Roll Character First");
+    }
+
+    public void onRollCharacterClicked()
+    {
+        Debug.Log("On Roll Character Button Click");
+        SceneManager.LoadScene("RollCharacterScene");
+        rolledCharacter = true;
+    }
+    
+    public void onQuitClicked()
     {
         // Add to onClick() for button. Choose this function
-        Debug.Log("On Exit Button Click");
+        Debug.Log("On Quit Button Click");
     }
 }
