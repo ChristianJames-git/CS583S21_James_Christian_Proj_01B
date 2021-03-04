0024706d86851c8cb4d1abe83829b663f7b21db5
Christian James <cjames6305@sdsu.edu>
2021-03-04 12:53:10 -0800

    Moved Script to Scripts folder

---
+++ Assets/Scripts/MainSceneManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainSceneManager : MonoBehaviour
+{
+    public void onPlayClicked()
+    {
+        if (GameManager.Instance.rolledCharacter)
+        {
+            Debug.Log("On Play Button Click");
+            SceneManager.LoadScene("PlayScene");
+        }
+        else
+            Debug.Log("Roll Character First");
+    }
+
+    public void onRollCharacterClicked()
+    {
+        Debug.Log("On Roll Character Button Click");
+        SceneManager.LoadScene("RollCharacterScene");
+    }
+
+    public void onQuitClicked()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
